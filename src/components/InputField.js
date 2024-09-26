ebf198bfada2a851a668f81c0eb19fb4b4768b03
HJK013 <kimhj9995@naver.com>
2024-09-27 00:14:18 +0900

    Update InputField.js
    입력란에 오류 표시

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const InputField = ({ type, placeholder, setState, id}) => {
+
+  const handleChange = (e) => {
+    setState(e.target.value);
+  };
+  
+  return (
+    <Input
+      id={id}
+      type={type}
+      placeholder={placeholder}
+      onChange={handleChange}
+      hasError={hasError}
+    />
+  );
+};
+
 const Input = styled.input`
   padding: 10px;
   margin: 10px 0;
   border: 1px solid #ccc;
   border-radius: 4px;
-  width: 100%;
+  width: 90%;
 `;
-
-const InputField = ({ type, placeholder, value, onChange }) => {
-  return (
-    <Input
-      type={type}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-    />
-  );
-};
-
 export default InputField;
