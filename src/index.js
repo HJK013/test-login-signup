81770e5b1c1aecf5f5e4cf3d017e0f0c0d88f8db
HJK013 <kimhj9995@naver.com>
2024-09-30 17:53:36 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React  from 'react';
+import ReactDOM from 'react-dom/client';           
+import { BrowserRouter } from 'react-router-dom';  
 import App from './App';
+import {} from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
