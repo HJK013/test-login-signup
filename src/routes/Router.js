0ebc811638251fbe98357ea0f0cd9494d34f8456
HJK013 <kimhj9995@naver.com>
2024-10-10 20:02:17 +0900

    Update Router.js

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,14 +1,16 @@
 import React from "react";
-import {Route, Routes} from "react-router-dom";
-import Login from "../page/Login";
-import Signup from "../page/Signup";
+import { Route, Routes, HashRouter } from "react-router-dom";
+import LoginPage from "../page/LoginPage";
+import SignUpPage from "../page/SignUpPage";
 
 const Router = () => {
-    return(
-        <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-        </Routes>
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+            </Routes>
+        </HashRouter>
     );
 };
 
